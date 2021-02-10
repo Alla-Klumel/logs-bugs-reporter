ec32b07e56082b87155757c71021bc2e72b3a256
Shacharit, Alla <aklumel@gmail.com>
2021-02-10 23:15:22 +0200

    CW-62

---
+++ logs-bugs-reporter/logs-bugs-email-notifier/src/main/java/telran/logs/bugs/client/EmailProviderClient.java
@@ -0,0 +1,16 @@
+package telran.logs.bugs.client;
+
+import javax.validation.constraints.NotEmpty;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailProviderClient {
+	public String getEmailByArtifact(String artifact) {
+		//TODO communication with sync service for email
+		return null;
+	}
+
+	
+
+}
