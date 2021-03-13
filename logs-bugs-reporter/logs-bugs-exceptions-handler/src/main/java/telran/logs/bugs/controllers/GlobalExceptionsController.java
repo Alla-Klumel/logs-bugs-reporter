942678d57e7335f8203495af84b9fc38894d7bf3
Klumel Shacharit, Alla <aklumel@gmail.com>
2021-03-13 18:24:26 +0200

    CW-69

--- logs-bugs-reporter/logs-bugs-exceptions-handler/src/main/java/telran/logs/bugs/controllers/GlobalExceptionsController.java
+++ logs-bugs-reporter/logs-bugs-exceptions-handler/src/main/java/telran/logs/bugs/controllers/GlobalExceptionsController.java
@@ -1,42 +1,46 @@
 package telran.logs.bugs.controllers;
+
+
 import javax.validation.ConstraintViolationException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.slf4j.*;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import telran.logs.bugs.exceptions.DuplicatedExceptions;
+import telran.logs.bugs.exceptions.DuplicatedException;
 import telran.logs.bugs.exceptions.NotFoundException;
-import telran.logs.bugs.exceptions.ServerException;
 
 @RestControllerAdvice
 public class GlobalExceptionsController {
-	static Logger LOG= LoggerFactory.getLogger(GlobalExceptionsController.class);
+	static Logger LOG = LoggerFactory.getLogger(GlobalExceptionsController.class);
 	@ExceptionHandler(ConstraintViolationException.class)
-	
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	String constraintVioletionHandler(ConstraintViolationException e) {
-		return processingExceptions(e);
+String constraintViolationHandler(ConstraintViolationException e) {
+	return processingExceptions(e);
 	}
 
-	
-	@ExceptionHandler(DuplicatedExceptions.class)
+	@ExceptionHandler(DuplicatedException.class)
 	@ResponseStatus(HttpStatus.CONFLICT)
-	String duplicatedKeyHandler(DuplicatedExceptions e) {
+	String duplicatedKeyHandler(DuplicatedException e) {
 		return processingExceptions(e);
 	}
 	@ExceptionHandler(NotFoundException.class)
-	@ResponseStatus (HttpStatus.NOT_FOUND)
-	 String notFoundHandler(NotFoundException e) {
-		 return processingExceptions(e);
-	 }
-	
-	private String processingExceptions(Exception e) {
-		 LOG.error("exception class{}, message: {}", e.getClass().getSimpleName(),e.getMessage());
-		return e.getMessage(); 
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	String notFounHandler(NotFoundException e) {
+		return processingExceptions(e);
 	}
+	
+	
 
+private String processingExceptions(Exception e) {
+	LOG.error("exception class: {}, message: {}", e.getClass().getSimpleName(), e.getMessage());
+	return e.getMessage();
+}
 }
--- logs-bugs-reporter/logs-bugs-exceptions-handler/src/main/java/telran/logs/bugs/exceptions/DuplicatedExceptions.java
+++
@@ -1,8 +0,0 @@
-package telran.logs.bugs.exceptions;
-
-@SuppressWarnings("serial")
-public class DuplicatedExceptions extends RuntimeException {
-public DuplicatedExceptions(String message) {
-  super(message);
-}
-}
