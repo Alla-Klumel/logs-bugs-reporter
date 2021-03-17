2ef8bb725faa886377af73bedc5b77f947d7bca4
Klumel Shacharit, Alla <aklumel@gmail.com>
2021-03-17 13:22:40 +0200

    CW-70

--- logs-bugs-reporter/logs-bugs-email-notifier/src/main/java/telran/logs/bugs/EmailNotifierAppl.java
+++ logs-bugs-reporter/logs-bugs-email-notifier/src/main/java/telran/logs/bugs/EmailNotifierAppl.java
@@ -2,8 +2,7 @@ package telran.logs.bugs;
 
 import java.util.function.Consumer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -16,55 +15,62 @@ import telran.logs.bugs.client.EmailProviderClient;
 import telran.logs.bugs.dto.LogDto;
 
 @SpringBootApplication
-
 public class EmailNotifierAppl {
-	private static final String PROGRAMMER_GREETING_NAME ="Programmer";
-	private static final String ASSIGNER_GREETING_NAME ="Opened Bugs Assigner";
-			static Logger LOG= LoggerFactory.getLogger(EmailNotifierAppl.class);
+	private static final String PROGRAMMER_GREETING_NAME = "Programmer";
+	private static final String ASSIGNER_GREETING_NAME = "Opened Bugs Assigner";
+	static Logger LOG = LoggerFactory.getLogger(EmailNotifierAppl.class);
+	
 	@Autowired
-	EmailProviderClient emailClient;
+EmailProviderClient emailClient;
 	@Value("${app-subject:exception}")
 	String subject;
 	@Autowired
 	JavaMailSender mailSender;
 	public static void main(String[] args) {
-SpringApplication.run(EmailNotifierAppl.class, args);
+		SpringApplication.run(EmailNotifierAppl.class, args);
+
 	}
+	
 	@Bean
 	Consumer<LogDto> getExceptionsConsumer(){
-		return this::takeLogAndsendMail;
+		return this::takeLogAndSendMail;
 	}
-	 void takeLogAndsendMail(LogDto logDto) {
-		 String email = emailClient.getEmailByArtifact(logDto.artifact);
-String greetingName = PROGRAMMER_GREETING_NAME;
-if(email.isEmpty()) {
-	greetingName = ASSIGNER_GREETING_NAME;
-	LOG.warn("Not received email for artifact{}", logDto.artifact);
-	email= emailClient.getAssignerMail();
-	if(email==null || email.isEmpty()) {
-		LOG.error("email 'to' has been recieved neither from logs-bugs-email-provider or" 
-	+"from logs-bugs-assigner-mail-provider");
-		return;
+	void takeLogAndSendMail(LogDto logDto) {
+		LOG.debug("received log {}", logDto);
+		String email = emailClient.getEmailByArtifact(logDto.artifact);
+		String greetingName = PROGRAMMER_GREETING_NAME;
+		
+		if (email == null || email.isEmpty()) {
+			greetingName = ASSIGNER_GREETING_NAME;
+			LOG.warn("Not received email for artifact {}", logDto.artifact);
+			email = emailClient.getAssignerMail();
+			if(email == null || email.isEmpty()) {
+				LOG.error("email 'to' has been received neither from logs-bugs-email-provider nor "
+						+ "from logs-bugs-assigner-mail-provider");
+				return;
+			}
+		}
+		sendMail(logDto, email, greetingName);
+		
 		
 	}
-}
-sendMail(logDto, email, greetingName);
-
-	 }
 	private void sendMail(LogDto logDto, String email, String greetingName) {
-SimpleMailMessage  message = new SimpleMailMessage();
-message.setSubject(subject);
-message.setTo(email);
-String text = getText(logDto, greetingName);
-message.setText(text);
- mailSender.send(message);
- LOG.debug("sent mail 'to{}; subject{}, text{}", text);
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setSubject(subject);
+		message.setTo(email);
+		String text = getText(logDto, greetingName);
+		message.setText(text);
+		mailSender.send(message);
+		LOG.debug("sent mail 'to' {} ; subject {}, text {}", email, subject, text);
+		
 	}
+
 	private String getText(LogDto logDto, String greetingName) {
 		return String.format("Hello, %s\n Exception has been received\n"
 				+ "Date: %s\nException Type: %s\nArtifact: %s\nExplanation: %s",
 				greetingName, logDto.dateTime, logDto.logType, logDto.artifact,
 				logDto.result);
 	}
+	
 
 }
--- logs-bugs-reporter/logs-bugs-email-notifier/src/main/java/telran/logs/bugs/client/EmailProviderClient.java
+++ logs-bugs-reporter/logs-bugs-email-notifier/src/main/java/telran/logs/bugs/client/EmailProviderClient.java
@@ -1,19 +1,56 @@
 package telran.logs.bugs.client;
 
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class EmailProviderClient {
-	public String getEmailByArtifact(String artifact) {
-		//TODO communication with sync service for email programmer's email
-		return null;
-	}
-
-	public String getAssignerMail() {
-		// TODO communication with sync service for assigner's email
-		return null;
-	}
-
-	
-
+	static Logger LOG = LoggerFactory.getLogger(EmailProviderClient.class);
+	RestTemplate restTemplate = new RestTemplate();
+	@Value("${app-url-assigner-mail:xxxx}")
+	String urlAssignerBase;
+	@Value("${app-url-programmer-mail:xxxx}")
+	String urlMailArtifactBase;
+public String getEmailByArtifact(String artifact) {
+	String urlMailProvider = getUrlMailArtifact(artifact);
+	String res = getMail(urlMailProvider);
+	LOG.debug("Programmer mail is {}", res);
+	return res;
 }
+private String getUrlMailArtifact(String artifact) {
+	String res = urlMailArtifactBase + "/email/" + artifact;
+	LOG.debug("url for getting email by artifact is {}", res);
+	return res ;
+}
+public String getAssignerMail() {
+	String res;
+	String urlMailProvider = getUrlAssigner();
+	res = getMail(urlMailProvider);
+	LOG.debug("assigner email is {}", res);
+	return res;
+}
+private String getMail(String urlMailProvider) {
+	String res;
+	try {
+		ResponseEntity<String> response =
+				restTemplate.exchange(urlMailProvider, HttpMethod.GET, null, String.class);
+		res = response.getBody();
+	} catch (RestClientException e) {
+		LOG.error("request to url {} thrown exception {}", urlMailProvider, e.getMessage());
+		res = "";
+	}
+	return res;
+}
+private String getUrlAssigner() {
+	
+	String res = urlAssignerBase + "/mail/assigner";
+	LOG.debug("URL for getting assigner mail is {}", res);
+	return res ;
+}
+}
+
